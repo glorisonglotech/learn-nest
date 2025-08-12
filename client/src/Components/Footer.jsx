09e9dca07003d949f3951b8dc76f70082f49f664
Developer866 <opeyemijoseph866@gmail.com>
2025-08-12 02:14:55 +0100

    feat: enhance Dashboard layout and update Footer styling

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Footer() {
 
   return (
-    <div className="flex flex-col md:flex-row border gap-8 ">
+    <div className="flex flex-col md:flex-row border gap-8 mt-10 p-4 bg-gray-100">
       <div className="md:flex-1">
         <h1 className="text-xl font-bold">LearnNest</h1>
         <p className=":w-[50%] ">
